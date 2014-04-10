5dc960168eafba33f3e1192cdfefc6f8df16e2e6
Matt Relich <mrelich6@gmail.com>
2014-04-10 17:50:11 +0900

    Adding some more submission scripts

---
+++ run/submission/spicemieMC.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+infile="condor_files/spicemieTemplate.cf"
+
+############################
+# Pick the Standard Candle
+############################
+
+#SC="SC1"
+#arguments=(0 1 2 3 4 5 6)
+
+SC="SC2"
+arguments=(1 3 10 30 51 100)
+
+
+############################
+# Loop
+############################
+for arg in ${arguments[@]};
+do
+
+    cat ${infile} | sed 's/args/'${SC}' '${arg}'/' | sed 's/tag/'${SC}'_filter'${arg}'/' > temp.cf
+    condor_submit temp.cf
+    #cat temp.cf
+    rm temp.cf
+
+done
